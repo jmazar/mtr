3713329edc3cd740d842678119830dee6800e344
jmazar <jeffrey.mazar@gmail.com>
2012-12-16 18:16:28 -0800

    Changed name from framework to mtr and continued writing DataManager unit test.

--- framework/tests/DataManagerUnitTest.cpp
+++ framework/tests/DataManagerUnitTest.cpp
@@ -7,7 +7,7 @@ protected:
     virtual void SetUp() {
         data_manager_ = new DataManager();
     }
-    
+
     virtual void TearDown() {
         delete data_manager_;
     }
@@ -15,9 +15,13 @@ protected:
     DataManager * data_manager_;
 };
 
-TEST_F(DataManagerTest, AddSymbolNameTest)
+TEST_F(DataManagerTest, PublishSymbolTest)
 {
-    EXPECT_EQ(MTR_STATUS_SUCCESS, data_manager_->PublishSymbol(); 
-    
+    EXPECT_EQ(MTR_STATUS_SUCCESS, data_manager_->PublishSymbol("INTC"));
+
+    std::vector<std::string> symbols;
+
+    EXPECT_EQ(MTR_STATUS_SUCCESS, data_manager_->GetSymbolNames( symbols );
+    EXPECT_EQ(1, symbols.size());
 }
 
