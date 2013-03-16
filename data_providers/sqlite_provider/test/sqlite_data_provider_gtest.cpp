3ee00e172a48016005ee4f8da0b95bba3f587be8
Jeffrey Mazar <waagnh@gmail.com>
2013-03-16 15:23:28 -0700

    Skeleton of sqlite test.

---
+++ data_providers/sqlite_provider/test/sqlite_data_provider_gtest.cpp
@@ -0,0 +1,24 @@
+#include "gtest/gtest.h"
+#include "DataManager.h"
+#include "sqlite_data_provider.h"
+
+using namespace mtr;
+
+class SqliteDataProviderTest : public ::testing::Test
+{
+protected:
+    virtual void SetUp() {
+        data_manager_ = new DataManager();
+    }
+
+    virtual void TearDown() {
+        delete data_manager_;
+    }
+
+    DataManager *           data_manager_;
+};
+
+TEST_F(SqliteDataProviderTest, LoadDBTest)
+{
+    SqliteDataProivder provider;
+}
